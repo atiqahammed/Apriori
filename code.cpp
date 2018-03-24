0b322506dbefe35510e7973eda2db7a23fe2f3a4
atiq <atiqa@ubuntu.ubuntu-domain>
2018-03-24 22:00:23 +0600

    file read complete

--- code.cpp
+++ code.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <iostream>  
+#include <sstream>
 
 using namespace std;
+
+struct transaction {
+	string transactionID;
+	vector<string> products;
+};
+
+transaction stringToTransaction(string input) {
+	istringstream iss (input);
+	transaction newTransaction;
+	iss >> newTransaction.transactionID;
+	string product;
+	while(iss >> product) 
+		newTransaction.products.push_back(product);
+	return newTransaction;
+}
+
+void inputTransactions(vector<transaction> &transactions) {
+	string input;
+	while(getline(cin, input)) 
+		transactions.push_back(stringToTransaction(input));
+}
+
+
+
 
 
 int main(void) {
 
-	cout << "completed" << endl;
+	vector<transaction> transactions;
+
+	inputTransactions(transactions);
+	cout << transactions.size() << endl;
+	//cout << "completed" << endl;
 
 
 
